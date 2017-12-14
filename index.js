a2cb758c61930ec37fa9cf32fabf81de5f40695e
Cara Kane <cara.kane@gmail.com>
2017-12-14 13:56:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -89,12 +89,25 @@ class Employer {
    }
 
    mealTotals() {
-     console.log(this.deliveries())
-     let arr = this.deliveries().map(delivery => delivery.meal());
-     let mealCount = {};
-     arr.forEach(function(i) {
-      mealCount[i.id] = (mealCount[i.id]||0) + 1;});
-      console.log(mealCount)
-   return(mealCount);
+  //    console.log(this.deliveries())
+  //    let arr = this.deliveries().map(delivery => delivery.meal());
+  //    let mealCount = {};
+  //    arr.forEach(function(i) {
+  //     mealCount[i.id] = (mealCount[i.id]||0) + 1;});
+  //     console.log(mealCount)
+  //  return(mealCount);
+
+  let allMeals = this.deliveries().map(delivery => {
+    return delivery.meal();
+  });
+  let summaryObject = {};
+  allMeals.forEach(function(meal) {
+    summaryObject[meal.id] = 0;
+  });
+  allMeals.forEach(function(meal) {
+    summaryObject[meal.id] += 1;
+  });
+  return summaryObject;
+
     }
 }
