088a742b49a87181a2a1c97f592f1a456e8fe6f6
Cara Kane <cara.kane@gmail.com>
2017-12-14 13:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ class Employer {
   //   // return(counter)
   let arr = this.employees()
   console.log(arr)
-  console.log(arr[0].totalSpent().bind(this))
+  console.log(arr[0].totalSpent())
 
   console.log(arr.map(employee => employee.totalSpent()))
   // console.log(this.employees())
